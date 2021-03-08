ba393be7b33f57a361104a90f99fea67ea83a78b
SNarboni <sebastien.narboni@hotmail.fr>
2021-03-08 15:46:30 +0100

    fini

---
+++ db.js
@@ -0,0 +1,62 @@
+const mongoose = require("mongoose");
+mongoose.connect(
+  "mongodb://localhost:27017/garage",
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("Connected to MongoDB");
+  }
+);
+
+const carsSchema = new mongoose.Schema({
+  _id: Number,
+  brand: String,
+  model: String,
+  year: Number,
+  created: Date,
+});
+
+const carsModel = mongoose.model("cars", carsSchema);
+// carsModel.deleteMany({}).then(() => {
+//   carsModel.create([
+//     {
+//       _id: 1,
+//       brand: "Renault",
+//       model: "Espace",
+//       year: 1999,
+//     },
+//     {
+//       _id: 2,
+//       brand: "Renault",
+//       model: "Scenic",
+//       year: 2004,
+//     },
+//     {
+//       _id: 3,
+//       brand: "Peugeot",
+//       model: "308",
+//       year: 2017,
+//     },
+//   ]);
+// });
+
+carsModel.findById({ _id: 1 }).then((response) => console.log(response));
+
+// carsModel
+//   .updateOne(
+//     {
+//       model: "Espace",
+//     },
+//     {
+//       year: 2000,
+//     }
+//   )
+//   .then((response) => {
+//     console.log(response);
+//   });
+
+// carsModel.deleteMany({brand : 'Renault'}).then((response) => {console.log(response)})
+
+// carsModel.insertMany(
+//   [{ _id: 1, brand: "Aston Martin", model: "DB9", year: 2010 },
+//   { _id: 2, brand: "Range Rover", model: "Discovery Sport", year: 2017 },]
+// );
